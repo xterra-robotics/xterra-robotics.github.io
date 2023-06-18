a64320a720abc06af3ba7a95a68fe8ac150ab7e3
Nimesh Khandelwal <nimesh6798@gmail.com>
2023-06-18 23:10:17 +0530

    fixed navbar and its mobile form. More fixes incoming

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,17 @@
 var navbar = `
-<button id="bar" class="" onclick="myFunction();"><i class="fa fa-bars fa-1.5x" ></i></button>
-            <a class="Links hidden" href="/index.html">Home</a>
-            <!-- <a class="Links hidden" href="./pages/product.html">Product</a> -->
-            <div class="dropdown Links hidden">
-                <button class="dropbtn">Robots
-                </button>
-                <div class="dropdown-content">
-                    <a href="/robots/quadruped.html">Quadruped</a>
-                    <a href="/robots/actuator.html">Actuator</a>
-                </div>
-            </div>
-            <a class="Links hidden" href="/pages/open-positions.html">Career</a>
-            <a class="Links hidden" href="#footer">Contact</a>
+<button id="bar" class="btn btn-outline-warning" onclick="myFunction();"><i class="fa fa-bars" ></i></button>
+<div class="navbar-item"><a class="Links hidden" href="/index.html">Home</a></div>
+<div class="navbar-item">
+
+    <div class="dropdown Links hidden">
+        <button class="dropbtn">Products
+        </button>
+        <div class="dropdown-content">
+            <a href="/robots/quadruped.html">Quadruped</a>
+            <a href="/robots/actuator.html">Actuator</a>
+        </div>
+    </div>
+</div>
+<div class="navbar-item"><a class="Links hidden" href="/pages/open-positions.html">Career</a></div>
+<div class="navbar-item"><a class="Links hidden" href="#footer">Contact</a></div>
 `;
