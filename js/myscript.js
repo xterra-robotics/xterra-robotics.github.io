51b49aba6dbea92b061e4d1174d6176252b3dd87
Nimesh Khandelwal <nimesh6798@gmail.com>
2023-07-28 11:53:52 +0530

    website UI updates and bug fixes

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,20 @@
 var clicked = -1;
-function myFunction(){
-    clicked = clicked * (-1);
+function myFunction(event){
+    event.stopPropagation();
+    var targetElement = event.target;
+    if(targetElement.nodeName === 'I') {
+        targetElement = targetElement.parentNode;
+    }
+    var customAttribute = targetElement.getAttribute("custom");
+    console.log(customAttribute)
+    if(customAttribute === "test"){
+        clicked = -1 * clicked;
+    } else if(customAttribute === "product"){
+        clicked = 1;
+    }else {
+        clicked = -1;
+    }
+    // clicked = clicked * (-1);
     if(clicked===1){
         // console.log(clicked);
         var nav= document.querySelectorAll(".Links.hidden");
@@ -14,3 +28,22 @@ function myFunction(){
         })
     }
 }
+
+function closeButton(props){
+    
+    clicked = -1;
+    if(clicked===1){
+        // console.log(clicked);
+        var nav= document.querySelectorAll(".Links.hidden");
+        [].forEach.call(nav, function(e){
+            e.classList.remove("hidden");
+        })
+    }else{
+        var nav= document.querySelectorAll(".Links");
+        [].forEach.call(nav, function(e){
+            e.classList.add("hidden");
+        })
+    }
+}
+
+
--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,14 @@
 var navbar = `
-<button id="bar" class="btn btn-outline-warning" onclick="myFunction();"><i class="fa fa-bars" ></i></button>
-<div class="navbar-item"><a class="Links hidden" href="/index.html">Home</a></div>
+<button id="bar" class="btn btn-outline-warning" custom="test" onclick="myFunction(event);"><i class="fa fa-bars" ></i></button>
+<div class="navbar-item"><a class="Links fix hidden" href="/index.html">Home</a></div>
 <div class="navbar-item">
 
-    <div class="dropdown Links hidden">
-        <button class="dropbtn">Products
+    <div class="dropdown fix Links hidden">
+        <button class="dropbtn" custom="product" onclick="myFunction(event)">Products
         </button>
         <div class="dropdown-content">
             <b>Robots</b>
-            <a href="/robots/quaruped.html">Svan M2</a>
+            <a href="/robots/quadruped.html">Svan M2</a>
             <a href="/robots/quadruped_m1.html">Svan M1</a>
             <b>Actuators</b>
             <a href="/robots/actuator_a2.html">QDD A2</a>
@@ -16,6 +16,28 @@ var navbar = `
         </div>
     </div>
 </div>
-<div class="navbar-item"><a class="Links hidden" href="/pages/open-positions.html">Career</a></div>
-<div class="navbar-item"><a class="Links hidden" href="#footer">Contact</a></div>
+<div class="navbar-item"><a class="Links fix hidden" href="/pages/open-positions.html">Career</a></div>
+<div class="navbar-item"><a class="Links fix hidden" href="#footer">Contact</a></div>
 `;
+
+// var navbar = `
+// <button id="bar" class="btn btn-outline-warning"><i class="fa fa-bars" ></i></button>
+// <div class="navbar-item"><a class="Links fix hidden" href="/index.html">Home</a></div>
+// <div class="navbar-item">
+
+//     <div class="dropdown fix Links hidden">
+//         <button class="dropbtn">Products
+//         </button>
+//         <div class="dropdown-content">
+//             <b>Robots</b>
+//             <a href="/robots/quadruped.html">Svan M2</a>
+//             <a href="/robots/quadruped_m1.html">Svan M1</a>
+//             <b>Actuators</b>
+//             <a href="/robots/actuator_a2.html">QDD A2</a>
+//             <a href="/robots/actuator.html">QDD A1</a>
+//         </div>
+//     </div>
+// </div>
+// <div class="navbar-item"><a class="Links fix hidden" href="/pages/open-positions.html">Career</a></div>
+// <div class="navbar-item"><a class="Links fix hidden" href="#footer">Contact</a></div>
+// `;
